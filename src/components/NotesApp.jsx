dfb724c7bb1beda74ce4293427f457d4fa25fa40
ari.darmika <ari.darmika@gmail.com>
2025-05-04 15:59:09 +0800

    feat: add navigation page

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Navigation from "./Navigation";
+import { Route, Routes } from "react-router-dom";
+import HomePages from "../pages/HomePages";
 
 const NotesApp = () => {
   return (
     <div className="notes-app">
       <header>
         <h1>Notes App</h1>
+        <Navigation />
       </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePages />} />
+        </Routes>
+      </main>
     </div>
   );
 };
---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePages = () => {
+  return (
+    <section>
+      <h2>Note Lists</h2>
+    </section>
+  );
+};
+
+export default HomePages;
