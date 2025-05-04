c742e9a3c78d1e9c635a7d2d1f8e84fc48099695
ari.darmika <ari.darmika@gmail.com>
2025-05-04 15:54:28 +0800

    chore: initial react vite package

---
+++ src/components/NotesApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotesApp = () => {
+  return (
+    <div className="notes-app">
+      <header>
+        <h1>Notes App</h1>
+      </header>
+    </div>
+  );
+};
+
+export default NotesApp;
