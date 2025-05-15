19de114aa2529b1df63c41e503fb4ffa137926fb
Kadek Ari Dharmika <ari.darmika@gmail.com>
2025-05-15 13:48:50 +0800

    feat(note detail): add delete note functionality

--- src/pages/NoteDetail.jsx
+++ src/pages/NoteDetail.jsx
@@ -1,18 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { getNote } from "../utils/local-data";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteNote, getNote } from "../utils/local-data";
 import { showFormattedDate } from "../utils";
+import { MdDelete } from "react-icons/md";
 
 const NoteDetail = () => {
   const { noteId } = useParams();
+  const navigate = useNavigate();
 
-  const { body, createdAt, title } = getNote(noteId);
+  const { id, body, createdAt, title } = getNote(noteId);
+
+  const handleDeleteNote = () => {
+
+    deleteNote(id);
+    navigate("/");
+
+  };
 
   return (
-    <div>
+    <div className="note-detail">
       <span className="note-item__date">{showFormattedDate(createdAt)}</span>
       <h1>{title}</h1>
       <div>{body}</div>
+
+      <div className="note-delete-container">
+            <button className="delete-note-button" onClick={() => handleDeleteNote()}><MdDelete/></button>
+      </div>
     </div>
   );
 };
